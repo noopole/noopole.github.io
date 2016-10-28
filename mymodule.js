2e9c65747d7678568a97675d8d9a096eb188397b
Bruno Gachie <bruno_gachie@trimble.com>
2016-10-28 16:17:12 +0200

    mymodule.js

---
+++ mymodule.js
@@ -0,0 +1,23 @@
+console.log( 'main module' )
+
+//import y from './submodule.js'
+var y = 2 
+
+export default {
+    prop: 100,
+    func: function ( x )
+    {
+        console.log( 'x => x + y + this.prop + e1() + i1() with v=%s and y=%s and this.prop=%s and o1->%s ans i1->%s', x, y, this.prop, e1(), i1() )
+        return x + y + this.prop + e1() + i1()
+    }
+}
+
+export function e1 ()
+{
+    return 1000
+} 
+
+function i1 ()
+{
+    return 10000
+}
